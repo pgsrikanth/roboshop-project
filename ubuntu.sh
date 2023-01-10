3c82fbee5e892beb5f8013bace066b76ff68361d
srikanth <gowtham@1996.com>
2023-01-11 00:48:16 +0530

    my editing is done

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,5 +2,5 @@ apt-get update
 apt-get install apache2 -y
 cd /var/www/html
 sudo su & >index.html
-vi index.html & i & hello world & :wq
+vi index.html & i & 'hello world' & :wq
 service apache2 start
