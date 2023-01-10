d3e2d5fb8df81ade20a76ef38766e3778bfbec89
srikanth <gowtham@1996.com>
2023-01-10 11:58:38 +0530

    my editing is done

--- projet.sh
+++ projet.sh
@@ -1,5 +1,4 @@
  yum install nginx -y
- systemctl enable nginx
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip
  cd /usr/share/nginx/html
  rm -rf *
